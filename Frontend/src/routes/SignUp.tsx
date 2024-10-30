51854891709717964d3c3fb7a701e751f10d0e87
Ethan-png1 <et769620@ucf.edu>
2024-10-30 18:57:57 -0400

    First Add
    This is the route error branch

---
+++ Frontend/src/routes/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Form, Button, Card } from 'react-bootstrap';
+
+const App: React.FC = () => {
+  return (
+    <p>signUp</p>
+  );
+};
+  
+
+export default App
