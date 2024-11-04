3ea17dd2689a18d8e663e8761dcd8a1862ca8fc8
rohant20 <rtalagadadeevi@gmail.com>
2024-11-04 17:22:48 -0500

    Server Update

---
+++ Server/controllers/searchEmail.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const asyncHandler = require("express-async-handler");
+const { User } = require("../models/userModel");
+
+const getUser = asyncHandler(async (req, res) => {
+  try {
+    console.log(User.find({}));
+    const userInput = {
+      username: req.body.username,
+      password: req.body.password
+    }
+
+    console.log(userInput.username + " " + userInput.password);
+
+    const currUser = await User.findOne({
+      username: "",
+      password: userInput.password
+    });
+
+
+    console.log(currUser);
+
+    res.status(200).json(currUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+});
+
+module.exports = {
+  getUser
+}
